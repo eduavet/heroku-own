b52092c36df220210984e0b6c591e636045412b2
eduavet <eduardavetisyan@gmail.com>
2018-05-28 15:53:08 +0400

    halfway through

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const path = require("path");
+const fetch = require('node-fetch');
+const util = require('util');
 
 const app = express();
 
@@ -9,13 +12,13 @@ app.listen(process.env.PORT || 3000, () => {
 });
 
 app.get('/', (req, res) => {
-  res.end('Homepage with "GET"')
+  res.sendFile(path.join(__dirname+'/index.html'))
 });
 
 app.post('/', (req, res) => {
   if (req.body) {
     console.log(req.body);
-    res.end(req.body);
+    res.end(JSON.stringify(req.body));
   } else {
     res.end('Homepage with "POST" and no payload')
   }
@@ -28,8 +31,40 @@ app.get('/hooks', (req, res) => {
 app.post('/hooks', (req, res) => {
   if (req.body) {
     console.log(req.body);
-    res.end(req.body);
+    res.end(JSON.stringify(req.body));
   } else {
     res.end('Hooks page with "POST" and no payload');
   }
+})
+
+app.get('/access', (req, res) => {
+  const client = '41e60573e31f48af0084';
+  const secret = 'f3e5ec6ea357c57c4d0c1a9cb875c109ebbcc49a';
+  const redirect = 'http://localhost:3000/access';
+  const { code } = req.query;
+  // return res.end(`https://github.com/login/oauth/access_token?client_id=${client}&client_secret=${secret}&code=${code}`)
+  console.log(code);
+  console.log(`https://github.com/login/oauth/access_token?client_id=${client}&client_secret=${secret}&code=${code}`);
+  fetch(`https://github.com/login/oauth/access_token?client_id=${client}&client_secret=${secret}&code=${code}`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/xml'
+    }
+  })
+    .then(token => {
+      console.log(typeof token);
+      console.log(token);
+      // console.log(util.inspect(token, { showHidden: true, depth: null }));
+      return token
+    })
+    // .then(token => res.json(token))
+    .catch(console.error);
+})
+
+app.get('/finish', (req, res) => {
+  res.end(JSON.stringify(req.query));
+})
+
+app.get('/github-logo.png', (req, res) => {
+  res.sendFile(path.join(__dirname+'/github-logo.png'))
 })
